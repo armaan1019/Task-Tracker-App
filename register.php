888b7a5d01827bf2ff81ae6eb7ee9b1125b45812
Armaan Sharma <armaan192004@gmail.com>
2026-06-11 13:02:18 -0700

    Fixed minor SQL typo

--- register.php
+++ register.php
@@ -32,7 +32,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
       } else {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUEs (?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $username, $email, $hashedPassword);
 
         if($stmt->execute()) {
